5c3745bc89823d8e507efab9ddd2a80750c1e28b
akki1027 <akki10271995@gmail.com>
2020-07-21 22:09:47 +0900

    Practice JS 5

--- js/main.js
+++ js/main.js
@@ -194,4 +194,18 @@
     text.value = ''
     text.focus();
   });
+
+  document.getElementById('add-color').addEventListener('click', () => {
+    const colors = document.querySelectorAll('input');
+    let selectedColor;
+
+    colors.forEach(color => {
+      if (color.checked == true) {
+        selectedColor = color.value;
+        const li = document.createElement('li');
+        li.textContent = selectedColor;
+        document.getElementById('show-color').appendChild(li);
+      }
+    });
+  });
 }
